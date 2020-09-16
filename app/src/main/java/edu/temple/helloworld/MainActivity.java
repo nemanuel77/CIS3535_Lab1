e100c0832d424ee091cca6f4ceeef3593da2748a
nemanuel77 <tue77789@temple.edu>
2020-09-16 18:11:20 -0400

    button implemented, with some code to track how many times we've smashed the button in particular.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -2,7 +2,11 @@ package edu.temple.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(R.id.btnClickMe);
+        final TextView myText = findViewById(R.id.textView2);
+
+
+        myButton.setOnClickListener((new View.OnClickListener() {
+            int count = 0;
+
+            @Override
+            public void onClick(View v) {
+                count++;
+             Log.d("You clicked", "the button!");
+             myText.setText("You've clicked the RAM-eating button " + count + " times!");
+            }
+        }));
+
+
     }
 }
